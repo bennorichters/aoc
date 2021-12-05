7677367638dab3f41c57d716cbbd9bbf12c0e726
Benno Richters <b.richters@gmail.com>
2021-12-05 08:35:23 +0100

    not 2022 yet

---
+++ bin/aoc_20201225_1.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  // var lines = File('./tin').readAsLinesSync();
+  var lines = File('./in').readAsLinesSync();
+
+}
--- bin/aoc_20211205_2.dart
+++ bin/aoc_20211205_2.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+:mport 'dart:io';
 
 void main(List<String> arguments) {
   // var lines = File('./tin').readAsLinesSync();
