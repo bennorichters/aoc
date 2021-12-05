dd6ebb0e7b108050feeb602efb9571c1ce4294ed
Benno Richters <b.richters@gmail.com>
2021-12-05 10:03:06 +0100

    2020 day 25

--- bin/aoc_20201225_1.dart
+++ bin/aoc_20201225_1.dart
@@ -1,7 +1,38 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  // var lines = File('./tin').readAsLinesSync();
-  var lines = File('./in').readAsLinesSync();
+  // const pkCard = 5764801;
+  // const pkDoor = 17807724;
+  const pkCard = 18499292;
+  const pkDoor = 8790390;
 
+  const prime = 20201227;
+
+  int step(int val, int subject) => (val * subject) % prime;
+
+  int findLoopSize(int publicKey) {
+    const subject = 7;
+    int val = 1;
+    int result = 0;
+    while (val != publicKey) {
+      result++;
+      val = step(val, subject);
+    }
+
+    return result;
+  }
+
+  int calcKey(int loopSize, int subject) {
+    int result = 1;
+    for (var i = 0; i < loopSize; i++) {
+      result = step(result, subject);
+    }
+
+    return result;
+  }
+
+  int lsCard = findLoopSize(pkCard);
+  int lsDoor = findLoopSize(pkDoor);
+  print(calcKey(lsCard, pkDoor));
+  print(calcKey(lsDoor, pkCard));
 }
