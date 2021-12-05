b9ae40525d25cbad172a3ca3fce58a6f7de3463c
Benno Richters <b.richters@gmail.com>
2021-12-05 10:45:07 +0100

    using Point instead of own Coordinate

--- bin/aoc_20211205_2.dart
+++ bin/aoc_20211205_2.dart
@@ -1,17 +1,18 @@
 import 'dart:io';
+import 'dart:math';
 
 void main(List<String> arguments) {
   // var lines = File('./tin').readAsLinesSync();
   var lines = File('./in').readAsLinesSync();
 
-  Set<Coordinate> hit = {};
-  Set<Coordinate> overlap = {};
+  Set<Point> hit = {};
+  Set<Point> overlap = {};
   for (var line in lines) {
     var parts = line.split(' -> ');
-    var start = pairToCoord(parts[0]);
-    var end = pairToCoord(parts[1]);
+    var start = pairToPoint(parts[0]);
+    var end = pairToPoint(parts[1]);
 
-    var steps = lineCoords(start, end);
+    var steps = linePoints(start, end);
     for (var step in steps) {
       if (!hit.add(step)) overlap.add(step);
     }
@@ -20,37 +21,20 @@ void main(List<String> arguments) {
   print(overlap.length);
 }
 
-class Coordinate {
-  final int x;
-  final int y;
-
-  Coordinate(this.x, this.y);
-
-  @override
-  String toString() => '[$x, $y]';
-
-  @override
-  bool operator ==(other) =>
-      (other is Coordinate) && x == other.x && y == other.y;
-
-  @override
-  int get hashCode => x + 31 * y;
-}
-
-Coordinate pairToCoord(String pair) {
+Point pairToPoint(String pair) {
   var parts = pair.split(',');
-  return Coordinate(int.parse(parts[0]), int.parse(parts[1]));
+  return Point(int.parse(parts[0]), int.parse(parts[1]));
 }
 
-List<Coordinate> lineCoords(Coordinate start, Coordinate end) {
-  int diffX = (end.x - start.x).sign;
-  int diffY = (end.y - start.y).sign;
+List<Point> linePoints(Point start, Point end) {
+  int diffX = (end.x - start.x).floor().sign;
+  int diffY = (end.y - start.y).floor().sign;
 
-  List<Coordinate> result = [];
-  Coordinate step = Coordinate(start.x, start.y);
+  List<Point> result = [];
+  Point step = Point(start.x, start.y);
   while (step != end) {
     result.add(step);
-    step = Coordinate(step.x + diffX, step.y + diffY);
+    step = Point(step.x + diffX, step.y + diffY);
   }
 
   result.add(end);
