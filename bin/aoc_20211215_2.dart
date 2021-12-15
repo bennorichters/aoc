fdcf8abff71c64954dae98f43b2194e484018b19
Benno Richters <b.richters@gmail.com>
2021-12-15 17:38:33 +0100

    less vars, taking advantage that it's garantueed a square

--- bin/aoc_20211215_2.dart
+++ bin/aoc_20211215_2.dart
@@ -7,26 +7,23 @@ void main(List<String> arguments) {
   var lines = File('./tin').readAsLinesSync();
   // var lines = File('./in').readAsLinesSync();
 
-  var blockWidth = lines[0].length;
-  var blockHeight = lines.length;
-
-  var dangerMap = parseDangerMap(lines, blockWidth, blockHeight);
-
-  var maxX = blockWidth * 5 - 1;
-  var maxY = blockHeight * 5 - 1;
+  var blockSize = lines[0].length;
+  var dangerMap = parseDangerMap(lines, blockSize);
+  var endOfGrid = blockSize * 5 - 1;
 
   Set<Point> neighbours(Point p) {
     var result = <Point>{};
 
     if (p.x > 0) result.add(Point(p.x - 1, p.y));
-    if (p.x < maxX) result.add(Point(p.x + 1, p.y));
+    if (p.x < endOfGrid) result.add(Point(p.x + 1, p.y));
     if (p.y > 0) result.add(Point(p.x, p.y - 1));
-    if (p.y < maxY) result.add(Point(p.x, p.y + 1));
+    if (p.y < endOfGrid) result.add(Point(p.x, p.y + 1));
 
     return result;
   }
 
   int findPath() {
+    var end = Point(endOfGrid, endOfGrid);
     var start = Point(0, 0);
     var minDanger = <Point, int>{start: 0};
     var stack = <Point>[start];
@@ -38,12 +35,12 @@ void main(List<String> arguments) {
         var totalDanger = minDanger[node]! + dangerMap[n]!;
         if (!minDanger.containsKey(n) || totalDanger < minDanger[n]!) {
           minDanger[n] = totalDanger;
-          if (!stack.contains(n)) stack.add(n);
+          if (n != end && !stack.contains(n)) stack.add(n);
         }
       }
     }
 
-    return minDanger[Point(maxX, maxY)]!;
+    return minDanger[end]!;
   }
 
   print(findPath());
@@ -51,12 +48,11 @@ void main(List<String> arguments) {
 
 Map<Point, int> parseDangerMap(
   List<String> lines,
-  int blockWidth,
-  int blockHeight,
+  int blockSize,
 ) {
   var dangerMap = <Point, int>{};
-  for (var y = 0; y < blockHeight; y++) {
-    for (var x = 0; x < blockWidth; x++) {
+  for (var y = 0; y < blockSize; y++) {
+    for (var x = 0; x < blockSize; x++) {
       dangerMap[Point(x, y)] = int.parse(lines[y].substring(x, x + 1));
     }
   }
@@ -64,12 +60,12 @@ Map<Point, int> parseDangerMap(
   for (int blockY = 0; blockY < 5; blockY++) {
     for (int blockX = 0; blockX < 5; blockX++) {
       if (blockY > 0 || blockX > 0) {
-        for (var y = 0; y < blockHeight; y++) {
-          for (var x = 0; x < blockWidth; x++) {
-            var px = blockWidth * blockX + x;
-            var py = blockHeight * blockY + y;
-            var refX = blockWidth * (blockX - (blockX > 0 ? 1 : 0)) + x;
-            var refY = blockHeight * (blockY - (blockX > 0 ? 0 : 1)) + y;
+        for (var y = 0; y < blockSize; y++) {
+          for (var x = 0; x < blockSize; x++) {
+            var px = blockSize * blockX + x;
+            var py = blockSize * blockY + y;
+            var refX = blockSize * (blockX - (blockX > 0 ? 1 : 0)) + x;
+            var refY = blockSize * (blockY - (blockX > 0 ? 0 : 1)) + y;
             var refValue = dangerMap[Point(refX, refY)]!;
             dangerMap[Point(px, py)] = refValue == 9 ? 1 : refValue + 1;
           }
