d1c00cb7f99e8f3497631757fa5f9547f153932d
Benno Richters <b.richters@gmail.com>
2021-12-20 11:02:45 +0100

    some performance improvements

--- bin/aoc_20211219_1.dart
+++ bin/aoc_20211219_1.dart
@@ -11,38 +11,57 @@ void main() {
     var visited = <int>{};
     var result = <CubeCoordinate>{};
 
-    void compareScans(
-      int scanA,
+    Transformation? findTransformation(
+      int scanNrA,
+      int scanNrB,
+      CubeCoordinate beaconA,
       List<Transformation> steps,
     ) {
-      if (!visited.add(scanA)) return;
+      for (var rotation in rotations) {
+        for (var beaconB in scans[scanNrB]) {
+          var translation = beaconA - rotation.perform(beaconB);
 
-      print(scanA);
+          if (areOverlapping(
+            scans[scanNrA],
+            scans[scanNrB],
+            translation,
+            rotation,
+          )) {
+            var tr = Transformation(translation, rotation);
 
-      for (var scanB = 0; scanB < scans.length; scanB++) {
-        for (var sa in scans[scanA]) {
-          for (var r in rotations) {
-            for (var sb in scans[scanB]) {
-              var rotated = r.perform(sb);
-              var translation = sa - rotated;
-              var h = hits(scans[scanA], scans[scanB], translation, r);
-
-              if (h.length >= 12) {
-                var tr = Transformation(translation, r);
-
-                for (var toAdd in scans[scanB]) {
-                  var r = toAdd;
-                  r = tr.perform(r);
-                  for (var t in steps.reversed) {
-                    r = t.perform(r);
-                  }
-                  result.add(r);
-                }
-
-                var recSteps = List.of(steps)..add(tr);
-                compareScans(scanB, recSteps);
+            print('here: $scanNrA $scanNrB');
+            for (var toAdd in scans[scanNrB]) {
+              var r = toAdd;
+              r = tr.perform(r);
+              for (var t in steps.reversed) {
+                r = t.perform(r);
               }
+              result.add(r);
             }
+
+            return tr;
+          }
+        }
+      }
+
+      return null;
+    }
+
+    void compareScans(
+      int scanNrA,
+      List<Transformation> steps,
+    ) {
+      if (!visited.add(scanNrA)) return;
+
+      print(scanNrA);
+
+      for (var scanNrB = 0; scanNrB < scans.length; scanNrB++) {
+        for (var beaconA in scans[scanNrA]) {
+          var tr = findTransformation(scanNrA, scanNrB, beaconA, steps);
+          if (tr != null) {
+            var recSteps = List.of(steps)..add(tr);
+            compareScans(scanNrB, recSteps);
+            break;
           }
         }
       }
@@ -53,19 +72,6 @@ void main() {
   }
 
   solve();
-
-  // var tr1 = compareScans(0, 1);
-  // print(tr1);
-
-  // var tr2 = compareScans(1, 4);
-  // print(tr2);
-
-  // var h = hits(scans[1], scans[4], tr2!.translation, tr2.rotation);
-  // for (var e in h) {
-  //   var step1 = tr2.perform(e);
-  //   var step2 = tr1!.perform(step1);
-  //   print(step2);
-  // }
 }
 
 class Transformation {
@@ -79,20 +85,23 @@ class Transformation {
   String toString() => '$translation $rotation';
 }
 
-Set<CubeCoordinate> hits(
+bool areOverlapping(
   Set<CubeCoordinate> scanA,
   Set<CubeCoordinate> scanB,
   CubeCoordinate translation,
   Rotation rotation,
 ) {
-  var result = <CubeCoordinate>{};
+  var count = 0;
   for (var sa in scanA) {
     for (var sb in scanB) {
-      if (rotation.perform(sb) + translation == sa) result.add(sb);
+      if (rotation.perform(sb) + translation == sa) {
+        count++;
+        if (count == 12) return true;
+      }
     }
   }
 
-  return result;
+  return false;
 }
 
 List<Set<CubeCoordinate>> parseAllScanners(List<String> lines) {
