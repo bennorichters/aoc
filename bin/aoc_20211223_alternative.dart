9bc573565bc1dceed4286dacd960d496b1b7db90
Benno Richters <b.richters@gmail.com>
2021-12-30 07:54:38 +0100

    changed to array

--- bin/aoc_20211223_alternative.dart
+++ bin/aoc_20211223_alternative.dart
@@ -1,86 +1,64 @@
 void main() {
-  var gs1 = GameState(20, 17, 16, 3, 21, 18, 5, 1, 19, 15, 14, 10, 13, 11, 7, 6);
-  var gs2 = GameState(20, 17, 16, 3, 21, 18, 5, 3, 19, 15, 14, 10, 13, 11, 7, 6);
-  print(gs1.hashCode);
-  print(gs2.hashCode);
+  var s1 = GameState(
+      [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]);
+  var s2 = GameState(
+      [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]);
+
+  printCave(s1);
+  print('');
+  printCave(s2);
 }
 
-int choose(int n, int k) => (k == 0) ? 1 : (n * choose(n - 1, k - 1)) ~/ k;
+void printCave(GameState gs) {
+  const chars = ['.', 'A', 'B', 'C', 'D'];
+  String hallway = '';
+  for (int i = 0; i < 7; i++) {
+    hallway += chars[gs.cave[i]];
+    if (i >= 3 && i <= 6) hallway += '.';
+  }
+  print(hallway);
+  for (var b = 0; b < 4; b++) {
+    print('  ' +
+        chars[gs.cave[7 + b]] +
+        ' ' +
+        chars[gs.cave[11 + b]] +
+        ' ' +
+        chars[gs.cave[15 + b]] +
+        ' ' +
+        chars[gs.cave[19 + b]]);
+  }
+}
 
 class GameState {
-  // assert that:
-  // a1 > a2 > a3 > a4
-  // b1 > b2 ...
-  // c1 > c2 ...
-  // d1 > d2 ...
-  // all numbers are smaller than or equal to 22 
-  // all numbers are greater than or equal to 0
-  // all numbers are unique
-  final int a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4;
+  final List<int> cave;
   final int _hash;
-  GameState(
-    this.a1,
-    this.a2,
-    this.a3,
-    this.a4,
-    this.b1,
-    this.b2,
-    this.b3,
-    this.b4,
-    this.c1,
-    this.c2,
-    this.c3,
-    this.c4,
-    this.d1,
-    this.d2,
-    this.d3,
-    this.d4,
-  ) : _hash = _calcHash(
-          a1,
-          a2,
-          a3,
-          a4,
-          b1,
-          b2,
-          b3,
-          b4,
-          c1,
-          c2,
-          c3,
-          c4,
-          d1,
-          d2,
-          d3,
-          d4,
-        );
+  GameState(this.cave) : _hash = _calcHash(cave);
 
-  static int _calcHash(
-    int a1,
-    int a2,
-    int a3,
-    int a4,
-    int b1,
-    int b2,
-    int b3,
-    int b4,
-    int c1,
-    int c2,
-    int c3,
-    int c4,
-    int d1,
-    int d2,
-    int d3,
-    int d4,
-  ) {
-    return (_enumeratedDistribution(a1, a2, a3, a4) << 42) +
-        (_enumeratedDistribution(b1, b2, b3, b4) << 28) +
-        (_enumeratedDistribution(c1, c2, c3, c4) << 14) +
-        _enumeratedDistribution(d1, d2, d3, d4);
+  static int _calcHash(cave) {
+    assert(cave.length == 23);
+    var aa = [0, 0, 0, 0];
+    var bb = [0, 0, 0, 0];
+    var cc = [0, 0, 0, 0];
+    var dd = [0, 0, 0, 0];
+    var ai = 0, bi = 0, ci = 0, di = 0;
+
+    for (int i = 22; i >= 0; i--) {
+      if (cave[i] == 1) aa[ai++] = i;
+      if (cave[i] == 2) bb[bi++] = i;
+      if (cave[i] == 3) cc[ci++] = i;
+      if (cave[i] == 4) dd[di++] = i;
+    }
+    assert(ai == 4 && bi == 4 && ci == 4 && di == 4);
+
+    return (_enumeratedDistribution(aa) << 42) +
+        (_enumeratedDistribution(bb) << 28) +
+        (_enumeratedDistribution(cc) << 14) +
+        _enumeratedDistribution(dd);
   }
 
   // max value is 8854, which fits in a 14 bits binary number
-  static int _enumeratedDistribution(int p1, int p2, int p3, int p4) =>
-      _nChoose4[p1] + _nChoose3[p2] + _nChoose2[p3] + p4;
+  static int _enumeratedDistribution(List<int> nrs) =>
+      _nChoose4[nrs[0]] + _nChoose3[nrs[1]] + _nChoose2[nrs[2]] + nrs[3];
 
   // https://en.wikipedia.org/wiki/Hash_function#Identity_hash_function
   @override
@@ -89,7 +67,7 @@ class GameState {
   @override
   bool operator ==(other) => other is GameState && other._hash == _hash;
 
-  // for example: _nChoose2[10] has the value: 10 choose 2 (=45)  
+  // for example: _nChoose2[10] has the value: 10 choose 2 (=45)
   static const _nChoose2 = [
     0,
     0,
